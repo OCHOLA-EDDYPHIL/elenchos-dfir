0fe42d911752f54e49e0a07c5048d479b8b9cb97
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2026-05-23 18:15:10 +0000

    feat(parser): add M2 parser contracts

--- src/siftguard/parser/mft.py
+++ src/siftguard/parser/mft.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
+from siftguard.parser.result import ParserResult
+
 PARSER_NAME = "mft_parser"
 EXPECTED_ARTIFACT_TYPE = "mft"
 
 
-def parse_mft(*args, **kwargs):
+def parse_mft(*args, **kwargs) -> ParserResult:
     raise NotImplementedError("Parser wrapper not implemented yet. This will be completed in M2.")
